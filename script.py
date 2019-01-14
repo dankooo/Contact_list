3b1ed0cfefaee8547a8cb186da1991c4ff65ae13
Danil Filippov <Sniper9543@gmail.com>
2019-01-15 01:55:29 +0300

    another commit

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import pickle
 
 cont_list='cont_list.data'
 
-class contact: 
+'''class contact: 
 	cont = {}
 	contacts_count = 0
 	def __init__(self,name,phone_number):
@@ -13,21 +13,28 @@ class contact:
 		print('В записной книжке {0} контактов'.format(contact.contacts_count))
 		contact.cont[self.name] = self.phone_number
 		print(contact.cont)
-		'''f = open(cont_list, 'wb')
+		
+		with open (cont_list, 'ab') as f:
+			pickle.dump(contact.cont,f)
+
+		
+		
+		
+		f = open(cont_list, 'wb')
 		pickle.dump('Количество контактов: ',contact.contacts_count,'\nКонтакты:',contact.cont,'\n',f)
 		f.close()
 		f = open(cont_list, 'rb')   #тут у меня говно4
 		read_list = pickle.load(f)
 		print(read_list)
 		f.close()'''
-
 	
 
 def add_contact():
-	s =[]
+	pass
+	'''s =[]
 	s.append(input('Введите Имя контакта: '))
 	s.append(input('Введите номер телефона: '))
-	contact(s[0],s[1])
+	contact(s[0],s[1])'''
 
 def edit_contact():
 	pass
@@ -37,7 +44,16 @@ def find_contact():
 
 def delete_contact():
 	pass
+ 
 
-s = int(input('input num'))
-menu = {1:add_contact, 2:edit_contact, 3:find_contact, 4:delete_contact}
-menu[s]()
+s = int(input('input num: '))
+menu = {1:'add_contact', 2:'edit_contact', 3:'find_contact', 4:'delete_contact'}
+menu2 = {11:'add_contact', 22:'edit_contact', 33:'find_contact', 44:'delete_contact'}
+'''menu[s]()'''
+
+with open (cont_list, 'ab') as f:
+	pickle.dump(menu,f)
+	pickle.dump(menu2,f)
+with open (cont_list, 'rb') as f:
+	read_list = pickle.load(f)
+	print(read_list)
