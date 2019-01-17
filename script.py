a06461e097f3f7c7fd787b2e0ba06a07bc62730c
Danil Filippov <Sniper9543@gmail.com>
2019-01-17 03:16:30 +0300

    another commit

--- script.py
+++ script.py
@@ -3,6 +3,12 @@ import os
 
 cont_list='cont_list.data'
 db ={}
+if os.path.getsize(cont_list)>0:
+	with open(cont_list, 'rb') as f:
+		db = pickle.load(f)
+else: 
+	with open (cont_list, 'wb') as f:
+			pickle.dump(db,f)
 
 class contact: 
 	cont = {}
@@ -14,8 +20,8 @@ class contact:
 			self.phone_number = phone_number
 			self.name = name
 		self.email = email
-		print('создан контакт "{0}", номер телефона: {1}, E-mail: {2}'.format(self.name, self.phone_number, self.email))
-		contact.cont = {'name':self.name, 'phone number':self.phone_number, 'E-mail':self.email}
+		print('создан контакт "{0}", номер телефона: {1}, E-mail: {2}'.format(self.name, self.phone_number, self.email), end = '\n')
+		contact.cont = {'Имя':self.name, 'Номер телефона':self.phone_number, 'E-mail':self.email}
 
 		if os.path.getsize(cont_list)>0:
 			with open(cont_list, 'rb') as f: 
@@ -23,24 +29,25 @@ class contact:
 				db = pickle.load(f)
 		
 		db[self.name] = contact.cont
-		print('Записано {0} контактов'.format(len(db)))
+		print('Записано {0} контактов'.format(len(db)), end = '\n')
 
 
 		with open (cont_list, 'wb') as f:
 			pickle.dump(db,f)
-			print(db)
 
 	def del_cont(name):
-		wrn = input('r u rly wanna del "{0}"? [Y/N]'.format(name))
+		wrn = input('Вы действительно хотите удалить контакт "{0}"? [Y/N]'.format(name))
 		if wrn == 'N':
 			return
-		if os.path.getsize(cont_list)>0:
-			with open(cont_list, 'rb') as f: 
-				global db    
-				db = pickle.load(f)
+		elif wrn == 'Y':
+			if os.path.getsize(cont_list)>0:
+				with open(cont_list, 'rb') as f: 
+					global db    
+					db = pickle.load(f)
+		else: return
 		
 		del db[name] 
-		print('Записано {0} контактов'.format(len(db)))
+		print('Записано {0} контактов'.format(len(db)), end = '\n')
 
 		with open (cont_list, 'wb') as f:
 			pickle.dump(db,f)
@@ -51,10 +58,17 @@ class contact:
 			with open(cont_list, 'rb') as f: 
 				global db    
 				db = pickle.load(f)
-				print(db)
-			
-		db[name][names_key] = input('Введите значение: ')
-		print(db)
+		for keys in db:
+			if name == keys:
+				subkeys = db[keys]
+				del db[name]
+				key = input('Введите значение: ')
+				subkeys[names_key] = key
+				db[key] = subkeys
+				break
+		else:
+			db[name][names_key] = input('Введите значение: ')
+		print('Контакт изменен:',key, '=>', db[key], end = '\n')
 
 		with open (cont_list, 'wb') as f:
 			pickle.dump(db,f)	
@@ -66,46 +80,78 @@ class contact:
 				db = pickle.load(f)
 		for values in db:
 			if values == value:
-				print(value, '=>', db.get(value))
-		'''for key in db.keys():
-			if key == value:'''
-			#else: return
+				print(value, '=>', db.get(value), end = '\n')
+			else: print('Контакт не найден')
 
+
+def show_contacts():
+	if os.path.getsize(cont_list)>0:
+		with open(cont_list, 'rb') as f:
+			db = pickle.load(f)
+			print('Записано {0} контактов'.format(len(db)), end='\n')
+		for keys in db:
+			print(keys, '=>', db[keys])
+	else: print('Записано {0} контактов'.format(len(db)), end='\n')
+	print('\n')
+	input('Нажмите любую клавишу')
+	print('\n')
+	
 def add_contact():
 	s =[]
 	s.append(input('Введите имя: '))
-	print (s)
 	inpt = input('Введите номер телефона: ')
 	while not inpt.isdigit():
-		print('Повторите ввод')
+		print('Повторите ввод', end = '\n')
 		inpt = input('Введите номер телефона: ')
 	s.append(inpt)
-	print (s)
 	s.append(input('Введите E-mail: '))
-	print (s)
 	contact(s[0],s[1],s[2])
+	print('\n')
+	input('Нажмите любую клавишу')
+	print('\n')
 
 def edit_contact():
 	s =[]
 	s.append(input('Введите Имя контакта: '))
-	s.append(input('Введите, какое значение нужно изменить (name, phone number, E-mail): '))
+	s.append(input('Введите, какое значение нужно изменить (Имя, Номер телефона, E-mail): '))
 	contact.edit_cont(s[0],s[1])
+	print('\n')
+	input('Нажмите любую клавишу')
+	print('\n')
 
 
 def find_contact():
 	s = input('Введите значение: ')
 	contact.find_cont(s)
+	print('\n')
+	input('Нажмите любую клавишу')
+	print('\n')
 
 
 def delete_contact():
 	s = input('Введите Имя контакта: ')
 	contact.del_cont(s)
+	print('\n')
+	input('Нажмите любую клавишу')
+	print('\n')
 
 
 s = 1
 while s !=0:
-	s = int(input('input num: '))
+	s = int(input('''Контакты
+	
+Меню:
+1) Создать новый контакт
+2) Изменить контакт
+3) Найти контакт
+4) Удалить контакт
+5) Список контактов
+0) Завершить работу
+
+Введите номер пункта меню: '''
+	))
+	print('\n')
 	if s == 0:
 		break
-	menu = {1:add_contact, 2:edit_contact, 3:find_contact, 4:delete_contact}
+	menu = {1:add_contact, 2:edit_contact, 3:find_contact, 4:delete_contact, 5:show_contacts}
 	menu[s]()
